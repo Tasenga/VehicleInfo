4386034522a22945ac6e4240e54141d2c946fd82
Anastasia Orlovskaya <nastassia.orlovskaya@gmail.com>
2020-07-15 12:44:18 +0300

    test of types in hive table has been added

--- vis/tests/test_ddl.py
+++ vis/tests/test_ddl.py
@@ -1,98 +1,84 @@
 from pathlib import Path
 from os import remove
 from typing import List, Any
+from decimal import Decimal
+from types import ModuleType
 
 from pyspark.sql import SparkSession
+import pytest
 
 from vis.configuration import Configuration
 from vis.ddl_processing import DDL
 
 
-# def test_update_ddl() -> None:
-#
-#     configuration = Configuration(
-#         mode='full',
-#         source_folder='test_data_source',
-#         db_name='schwacke_test',
-#         host='localhost',
-#         port=27017,
-#         current_date='2020-07-14',
-#         current_timestamp='1594726338',
-#     )
-#
-#     try:
-#         ddl = DDL(configuration)
-#         ddl.update_ddl(cwd=Path("D:\\project\\VehicleInfo"))
-#         with Path(ddl.mode_directory,
-#              f'{ddl.configuration.mode.value}_schwacke_hive_tables_ddl.txt').open(
-#             'r'
-#         ) as ddl_file:
-#             text_ddl = ddl_file.read()
-#
-#         with Path(ddl.mode_directory,
-#                   f'control_schwacke_hive_tables_ddl.txt').open(
-#             'r'
-#         ) as control_ddl_file:
-#             control_ddl = control_ddl_file.read()
-#     finally:
-#         remove(Path(ddl.mode_directory,
-#              f'{ddl.configuration.mode.value}_schwacke_hive_tables_ddl.txt'))
-#
-#     assert text_ddl == control_ddl
+spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
 
-def test_run_ddl(spark_session: SparkSession) -> None:
+def teardown_module(module: ModuleType) -> None:
+    spark.stop()
+    print('spark-session close')
+
+
+def test_update_ddl() -> None:
 
     configuration = Configuration(
-        mode='short',
+        mode='full',
         source_folder='test_data_source',
-        db_name='schwacke_test_type',
+        db_name='schwacke_test',
         host='localhost',
         port=27017,
-        current_date='test',
-        current_timestamp='1',
+        current_date='2020-07-14',
+        current_timestamp='1594726338',
     )
-    ddl = DDL(configuration)
-    cwd = str(Path.cwd()).replace('\\', '/')
+
     try:
-        with Path(ddl.mode_directory, 'test_type.gkp').open('w') as gkp:
-            gkp.write('''ab\t4\t1986546\t8000.65\n\taa\t4\ta\n''')
+        ddl = DDL(configuration)
+        ddl.update_ddl(cwd=Path("D:\\project\\VehicleInfo"))
+        with Path(ddl.mode_directory, f'{ddl.configuration.mode.value}_schwacke_hive_tables_ddl.txt').open(
+            'r'
+        ) as ddl_file:
+            text_ddl = ddl_file.read()
 
-        with ddl.file_ddl.open('w') as ddl_file:
-            ddl_file.write(
-                f'''CREATE DATABASE IF NOT EXISTS {ddl.configuration.db_name}
-                    LOCATION "{cwd}/dbs/{ddl.configuration.db_name}";
-
-                    CREATE TABLE IF NOT EXISTS {ddl.configuration.db_name}.test_type (
-                        Varchar VARCHAR(2),
-                        SmallInteger SMALLINT,
-                        Integer INT,
-                        DEC DECIMAL(6, 2)
-                    )
-                    PARTITIONED BY(data_date_part STRING, data_timestamp_part STRING)
-                    ROW FORMAT DELIMITED
-                    FIELDS TERMINATED BY '\t'
-                    STORED AS TEXTFILE;
-
-                    LOAD DATA INPATH "{cwd}/{ddl.configuration.source_folder}/test_type.gkp"
-                    INTO TABLE {ddl.configuration.db_name}.test_type
-                    PARTITION(
-                        data_date_part='{ddl.configuration.current_date}',
-                        data_timestamp_part='{ddl.configuration.current_timestamp}');'''
-            )
-
-        ddl.run_ddl(spark_session)
-        print(f'create {ddl.configuration.db_name}')
-
-        test_type_table = spark_session.sql(f'''SELECT * FROM {ddl.configuration.db_name}.test_type;''')
-        test_type_table.show()
-
+        with Path(ddl.mode_directory, 'control_schwacke_hive_tables_ddl.txt').open('r') as control_ddl_file:
+            control_ddl = control_ddl_file.read()
     finally:
         remove(Path(ddl.mode_directory, f'{ddl.configuration.mode.value}_schwacke_hive_tables_ddl.txt'))
-        spark_session.sql(f'''DROP DATABASE {ddl.configuration.db_name} CASCADE''')
+
+    assert text_ddl == control_ddl
+
+
+@pytest.fixture(
+    scope="function",
+    params=[
+        (
+            "ab\t4\t1986546\t8000.65\n\taa\t4\ta",
+            spark.createDataFrame(
+                [["ab", 4, 1986546, Decimal(8000.65)], ['', None, 4, None]],
+                "Varchar: string, SmallInteger: smallint, Integer: int, DEC: Decimal(6, 2)",
+            ).collect(),
+        ),
+        (
+            "abcd\t10456564\t0\t800",
+            spark.createDataFrame(
+                [["ab", None, 0, Decimal(800)]],
+                "Varchar: string, SmallInteger: smallint, Integer: int, DEC: Decimal(6, 2)",
+            ).collect(),
+        ),
+        (
+            "AS\t4S\t054668878\t80055.55",
+            spark.createDataFrame(
+                [["AS", None, 54668878, None]],
+                "Varchar: string, SmallInteger: smallint, Integer: int, DEC: Decimal(6, 2)",
+            ).collect(),
+        ),
+    ],
+)
+def param_test(request: pytest.fixture) -> List[Any]:
+    return request.param
 
 
 class TestRunDDL:
+
     configuration = Configuration(
         mode='short',
         source_folder='test_data_source',
@@ -105,10 +91,7 @@ class TestRunDDL:
     ddl = DDL(configuration)
     cwd = str(Path.cwd()).replace('\\', '/')
 
-    def setup(self, data: List[Any]) -> None:
-        with Path(self.ddl.mode_directory, 'test_type.gkp').open('w') as gkp:
-            gkp.write('''ab\t4\t1986546\t8000.65\n\taa\t4\ta\n''')
-
+    def setup(self) -> None:
         with self.ddl.file_ddl.open('w') as ddl_file:
             ddl_file.write(
                 f'''CREATE DATABASE IF NOT EXISTS {self.ddl.configuration.db_name}
@@ -132,12 +115,17 @@ class TestRunDDL:
                         data_timestamp_part='{self.ddl.configuration.current_timestamp}');'''
             )
 
-    def teardown(self, spark_session: SparkSession) -> None:
+    def teardown(self) -> None:
         remove(Path(self.ddl.mode_directory, f'{self.ddl.configuration.mode.value}_schwacke_hive_tables_ddl.txt'))
-        spark_session.sql(f'''DROP DATABASE {self.ddl.configuration.db_name} CASCADE''')
+        spark.sql(f'''DROP DATABASE {self.ddl.configuration.db_name} CASCADE''')
 
-    def test_correct_data_type(self, spark_session: SparkSession) -> None:
-        self.ddl.run_ddl(spark_session)
-        test_type_table = spark_session.sql(f'''SELECT * FROM {self.ddl.configuration.db_name}.test_type;''')
-        print(test_type_table.show())
-        assert 5 * 6 == 30
+    def test_correct_data_type(self, param_test: List[Any]) -> None:
+        (input, expected_output) = param_test
+        with Path(self.ddl.mode_directory, 'test_type.gkp').open('w') as gkp:
+            gkp.write(input)
+        self.ddl.run_ddl(spark)
+        test_type_table = spark.sql(
+            f'''SELECT Varchar, SmallInteger, Integer, DEC FROM {self.ddl.configuration.db_name}.test_type;'''
+        ).collect()
+        expected_type_table = expected_output
+        assert test_type_table == expected_type_table
